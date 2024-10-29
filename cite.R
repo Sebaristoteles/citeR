0175d48835d0b0bc33e6435c90331eed1ea96444
Sebaristoteles <sebastian.geschonke@gmail.com>
2024-10-29 16:40:43 +0100

    new version: from project folder to full bibtex file

--- cite.R
+++ cite.R
@@ -1,5 +1,11 @@
-library("tidyverse")
-library("purrr")
+packages_to_load <- c("tidyverse", "purrr", "magrittr", "pbapply", "here")
+
+invisible(
+  suppressPackageStartupMessages(
+    sapply(packages_to_load, 
+           function(pkg) eval(parse(text = paste0("library(", pkg, ")"))))
+  )
+)
 
 '%ni%' <- Negate("%in%")
 
@@ -19,17 +25,19 @@ library("purrr")
 
 
 citeR <- function(input, 
-                  output = "bibtex.bib", # change the location and file name
+                  output = here("software.bib"), # change the location and file name
                   exclude = NULL, # vector of packages to be excluded
                   dependencies = F){
   
   # ---------- 1) identify if file or folder and R-files ----------------------
   
-  if(grepl("\\.(R|Rmd)$", input)){
-    files_to_check <- input
-  }else{
-    files_to_check <-
+  if(grepl("\\.(R|Rmd)$", input)){ #  all from current folder
+    files_to_check <- 
       list.files(input) %>%
+      .[grepl("\\.(R|Rmd)$", .)]
+  }else{ #                            all files in down the folder structure
+    files_to_check <-
+      list.files(input, recursive = T) %>%
       .[grepl("\\.(R|Rmd)$", .)]
   }
   
@@ -42,18 +50,22 @@ citeR <- function(input,
   # ---------- 2) read all R-files and prepare --------------------------------
   
   files_read <- list()
+  files_paths <- list()
   
   if(length(files_to_check) == 1){
     files_read[[1]] <- 
-      read_file(input) %>%
-      strsplit(., "\r\n") %>%
-      unlist()
+      read_file(input) #%>%
+      #strsplit(., "\r\n") %>%
+      #unlist()
   }else{
     for(i in 1:length(files_to_check)){
+      files_paths[[i]] <-
+        file.path(input, files_to_check[i])
+      
       files_read[[i]] <- 
-        read_file(file.path(input, files_to_check[i])) %>%
-        strsplit(., "\r\n") %>%
-        unlist()
+        read_file(files_paths[[i]]) #%>%
+        #strsplit(., "\r\n") %>%
+        #unlist()
     }
   }
   
@@ -61,19 +73,86 @@ citeR <- function(input,
   
   # ---------- 3) identify packages -------------------------------------------
   
-  packages_found <- list()
-  for(i in 1:length(files_read)){
-    packages_found[[i]] <- 
-      regmatches(files_read[[i]], 
-                 gregexpr("(library|require)\\(.+?\\)", files_read[[i]])) %>%
+  packages_R_has <- available.packages() %>% as.data.frame()
+  
+  
+  # Function to get all potential packages
+  
+  f_get_potential_packages <- function(text){
+    
+    # get all potential packages
+    lib_req <-
+      regmatches(text, 
+                 gregexpr("(library|require)\\(.+?\\)", text)) %>%
       unlist() %>%
       gsub('library|require|\\(|\\)|\\"', "", .) %>%
-      gsub("\\'", "", .)
+      gsub("\\'", "", .) %>%
+      unique()
+    
+    all_enclosed_items <-
+      c(regmatches(text, 
+                   gregexpr("\'.+?\'", text)) %>%
+          unlist(),
+        regmatches(text, 
+                   gregexpr('\".+?\"', text)) %>%
+          unlist(),
+        regmatches(text, 
+                   gregexpr("\\(.+?\\)", text)) %>%
+          unlist()
+      ) %>%
+      gsub("\'|\"|\\(|\\)", "", .) %>%
+      unique()
+    
+    potential_packages <-
+      c(lib_req, all_enclosed_items) %>%
+      unique()
+    
+    return(potential_packages)
   }
   
+  
+  # Function to find exact matches for each set of texts
+  f_find_exact_matches <- function(texts, patterns = packages_R_has$Package) {
+    # Create a data frame to identify exact matches between patterns and the current set of texts
+    map_dfr(patterns, ~ {
+      tibble(
+        pattern = .x,
+        text = texts,
+        match = texts == .x
+      )
+    }) %>%
+      filter(match)
+  }
+  
+  
+  # get all potential packages from all files
+  message(format(Sys.time(), "%d.%m.%Y %X"), 
+          ": getting all potential packages")
+  pot_packs <-
+    pblapply(files_read, f_get_potential_packages)
+  
+  
+  # Apply the function across all text sets with a progress bar
+  message(format(Sys.time(), "%d.%m.%Y %X"), 
+          ": finding matches of potential packages and existing matches")
+  results_list <- pblapply(pot_packs[1:2], f_find_exact_matches)
+  
+  packages_found <-
+    results_list %>%
+    unlist() %>%
+    unique()
+  
+  # check final results once more
+  message(format(Sys.time(), "%d.%m.%Y %X"), 
+          ": confirming matches")
+  packages_found_confirmed <-
+    f_find_exact_matches(packages_found) %>%
+    filter(match %in% TRUE) %>%
+    pull(pattern)
+  
   packages_to_cite <-
-    packages_found %>%
-    unlist()
+    c(packages_found_confirmed, packages_to_load) %>%
+    unique()
   
   # exclude packages provided as a vector by the user
   if(!is.null(exclude)){
@@ -83,11 +162,12 @@ citeR <- function(input,
   
   
   # check if all packages are installed
-  # otherwise raise error
+  # otherwise raise warning
   if(any(packages_to_cite %ni% installed.packages())){
     missing_p <- packages_to_cite[packages_to_cite %ni% installed.packages()]
-    stop("The following packages need to be installed or excluded:\n",
-         paste0(missing_p, collapse = "\n"))
+    warning("The following packages are not installed and might be false Positives:\n",
+         paste0(missing_p, collapse = "\n"),
+         "\nConsider to exclude them.")
   }
   
   # order alphabetically
@@ -120,7 +200,7 @@ citeR <- function(input,
     gsub("title = \\{.+?\\}", 
          "title = {citeR: An easy citation tool to account for all your used packages}", .) %>%
     gsub("author = \\{.+?\\}", "author = {Sebastian Geschonke", .) %>%
-    gsub("year = \\{.+?\\}", "year = {2022}", .) %>%
+    gsub("year = \\{.+?\\}", "year = {2024}", .) %>%
     gsub("url = \\{.+?\\}", "url = {https://github.com/Sebaristoteles/citeR}",.) %>%
     gsub("organization.+?$", "", .) %>%
     gsub("address.+?$", "", .) %>%
